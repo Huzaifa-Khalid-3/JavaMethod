5572ac26a7b8a66b2b42c307bf9cb35e01017afb
Huzaifa-Khalid-3 <161176873+Huzaifa-Khalid-3@users.noreply.github.com>
2024-02-26 15:10:13 -0500

    Update Code.java

--- Code.java
+++ Code.java
@@ -1,1 +1,29 @@
+package JavaMethods;
+import java.util.*;
+public class Main {
 
+	public static void main(String[] args) {
+		Scanner k = new Scanner(System.in);
+		
+		int num1;
+		int num2;
+		System.out.println("Enter num 1: ");
+		num1 = k.nextInt();
+		System.out.println("Enter num 2: ");
+		num2 = k.nextInt();
+		System.out.println("sum is: "+addNumbers(num1,num2));
+		
+		System.out.println("product is: "+subtractNumbers(num1,num2));
+
+	}
+	
+	static int addNumbers(int x, int y) {
+		return x+y;
+	}
+	
+	static int subtractNumbers(int x, int y) {
+		return x-y;
+	}
+	
+
+}
